3eb141c512d61dfc48771275da8bba085aae3bf0
Balakrishna Ette <balakrishna.ette@outlook.com>
2022-07-06 09:00:50 +0530

    Hello World (local properties & data binding)

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,5 +1,13 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-    
+    name; //undefined
+    fullname = "Salesforce Developer"; //string
+    age = 30; //number
+    location = {
+        city: "Houston",
+        country: "United States",
+        postalCode: "50033"
+    }; //object
+    fruits = ["Banana", "Orange", "Pomogranite", "Pineapple"]; //array
 }
