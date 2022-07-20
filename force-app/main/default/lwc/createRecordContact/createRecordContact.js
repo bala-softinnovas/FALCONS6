7ca8e1d1bdac80b653687d6abac8d16a1bad7098
Balakrishna Ette <balakrishna.ette@outlook.com>
2022-07-20 09:08:57 +0530

    Create Record UC1

--- force-app/main/default/lwc/createRecordContact/createRecordContact.js
+++ force-app/main/default/lwc/createRecordContact/createRecordContact.js
@@ -1,10 +1,28 @@
 import { LightningElement } from 'lwc';
 
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import { createRecord } from 'lightning/uiRecordApi';
+
 export default class CreateRecordContact extends LightningElement {
-    fields = {};
+    formdata = {};
     changeHandler(event) {
         const {name, value} = event.target;
-        this.fields[name] = value;
-        console.log(this.fields);
+        this.formdata[name] = value;
+        console.log(this.formdata);
+    }
+
+    createContact() {
+        const recordInput = {
+            apiName: CONTACT_OBJECT.objectApiName,
+            fields: this.formdata
+        }
+        createRecord(recordInput)
+            .then(result => {
+                console.log(result);
+                this.template.querySelector('form.contact').reset();
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 }
